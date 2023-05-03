38d904b819d5582380f71b89a42868945fad3e9f
RonnySk <ronny_sk@hotmail.com>
2023-05-03 09:36:51 +0200

    add variables at router.use

---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+const bcryptjs = require("bcryptjs");
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Flat = require("../models/Flat.model");
+const Task = require("../models/Task.model");
+
+
+
+
