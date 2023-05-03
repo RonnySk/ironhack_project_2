60db8c87f2957f8211c6c1be09fe3a513de70108
RonnySk <ronny_sk@hotmail.com>
2023-05-03 10:13:24 +0200

    signup post route, not finished

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,10 +7,18 @@ const Task = require("../models/Task.model");
 // signup route
 
 router.get('/signup', (req, res) => {
-    res.render("signup")
+    res.render("signup");
 });
 
-// router.post()
+router.post('/signup', async (req,res) => {
+   
+        const salt = await bcryptjs.genSalt(12);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const newUser = await User.create({username: req.body.username, email: req.body.email, password: hash}); 
+        console.log(newUser)
+        res.render("homePage", newUser)
+
+})
 
 
 module.exports = router;
