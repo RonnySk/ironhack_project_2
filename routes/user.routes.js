aff48a2bd6c2f73fc2e7abb48070d137cfe0d080
RonnySk <ronny_sk@hotmail.com>
2023-05-03 09:54:06 +0200

    add user route at app.js and create signup route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,15 @@ const User = require("../models/User.model");
 const Flat = require("../models/Flat.model");
 const Task = require("../models/Task.model");
 
+// signup route
 
+router.get('/signup', (req, res) => {
+    res.render("signup")
+});
+
+// router.post()
+
+
+module.exports = router;
 
 
